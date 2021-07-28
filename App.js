98a4b231ed02d89bf3997af63d0984ce64996de0
belichenk0andreyka <belichenko.andreyka22@gmail.com>
2021-07-28 21:26:21 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {Button, SafeAreaView, Text, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Navigation} from 'react-native-navigation';
+
+const App: () => Node = props => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Text>app</Text>
+      <Button
+        title="Push Settings Screen"
+        color="#710ce3"
+        onPress={() =>
+          Navigation.push(props.componentId, {
+            component: {
+              name: 'Settings',
+              options: {
+                topBar: {
+                  title: {
+                    text: 'Settings',
+                  },
+                },
+              },
+            },
+          })
+        }
+      />
+    </SafeAreaView>
+  );
+};
+
+export default App;
